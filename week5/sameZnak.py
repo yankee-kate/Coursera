07f8273a497e5316e4dcff9e214106ed45b603e3
Ekaterina.Iankovaia <sautina3@gmail.com>
2019-03-21 09:51:11 +0300

    merge

--- week5/sameZnak.py
+++ week5/sameZnak.py
@@ -1,7 +1,8 @@
 def sameZnak(A):
     for i in range(len(A) - 1):
-        if (int(A[i + 1]) >= 0 and int(A[i]) >= 0) \
-                or (int(A[i + 1]) < 0 and int(A[i]) < 0):
+        if (int(A[i + 1]) >= 0 and int(A[i]) >= 0):
+            return A[i], A[i + 1]
+        elif (int(A[i + 1]) < 0 and int(A[i]) < 0):
             return A[i], A[i + 1]
     return ''
 
